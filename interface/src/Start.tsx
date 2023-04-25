93ff7d6307fa39d863dc4c83ad2cfbf291f90389
Aryan Purohit <aryan31.purohit@gmail.com>
2023-04-25 22:34:59 +0530

    aaaa...

--- interface/src/Start.tsx
+++ interface/src/Start.tsx
@@ -1,11 +1,30 @@
+// import { useState, useEffect } from "react";
+
 function Start() {
+  // const [payload, setPayload] = useState("");
+  // const [clicked, setClicked] = useState(false);
+
   const handleClick = () => {
     fetch("http://127.0.0.1:1880/coffee?data=coffee", {
       method: "GET",
     })
       .then(() => console.log("Coffee machine started!"))
       .catch((error) => console.log(error));
+    // setClicked(true);
   };
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const response = await fetch("http://127.0.0.1:1880/coffee");
+  //     const text = await response.text();
+  //     setPayload(text);
+  //   };
+  //   if (clicked) {
+  //     fetchData();
+  //     setClicked(false);
+  //   }
+  // }, [clicked]);
+
+  // console.log("Payload: ", payload);
 
   return (
     <button
