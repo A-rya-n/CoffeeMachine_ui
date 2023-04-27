4df1816419bd3d33a6508e2ad199650cddb05054
A-rya-n <aryan31.purohit@gmail.com>
2023-04-27 11:08:58 +0530

    minor change

--- interface/src/App.tsx
+++ interface/src/App.tsx
@@ -15,11 +15,18 @@ function App() {
     <div className="bg-blue-300 h-screen flex items-center justify-center">
       <ToggleNode nodeState={nodeState} onNode={ShowNode} />
       {nodeState ? (
-        <iframe
-          src="http://127.0.0.1:1880/"
-          width={"80%"}
-          height={"80%"}
-        ></iframe>
+        <>
+          <link
+            rel="stylesheet"
+            href="/home/dev/Desktop/CoffeeMachine_ui/interface/src/node.css"
+          />
+          <iframe
+            src="http://127.0.0.1:1880/"
+            // width={"80%"}
+            // height={"80%"}
+            sandbox={"allow-same-origin allow-scripts"}
+          ></iframe>
+        </>
       ) : (
         <div className="bg-white p-6 rounded-lg shadow-xl flex flex-col justify-evenly h-80 w-80">
           <h1 className="text-gray-900 font-bold text-2xl">Coffee Machine</h1>
